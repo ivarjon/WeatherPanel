677ebe815edf51d3bfaad60879aa3d8aab245691
ivarjon <ivar3145@gmail.com>
2021-12-13 19:56:10 +0000

    Working skeleton established

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { ChangeEvent } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+function Weather() {
+  const key = "cde24c243c012a396950f571bcdd4836";
+  const [input, setInput] = useState('');
+  const [city, setCity] = useState('');
+  const [desc, setDesc] = useState('');
+  const [temp, setTemp] = useState('');
+
+  function handleSubmit(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" + key)
+    .then((response) => response.json())
+    .then(data => {
+      setCity(input);
+      setTemp(data['main']['temp']  + " °C");
+      setDesc(data['weather'][0]['description']);
+    })
+
+    .catch(err => alert("Invalid City Name"))
+  }
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.currentTarget.value);
+  };
+
+  return(
+    <div>
+      <input type="text" className="inputValue" placeholder="Search" onChange={onChange} />
+      <input type="submit" value="Submit" onClick={() => handleSubmit()} />
+      <div className="display">
+        <p id="name" />{city}<p/>
+        <p className="desc" />{desc}<p/>
+        <p className="temp" />{temp}<p/>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Weather;
