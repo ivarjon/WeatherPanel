3e6d330f0d594c197bb00275504d5668834bad76
ivarjon <ivar3145@gmail.com>
2021-12-13 20:24:08 +0000

    Removed unnecessary whitespacing

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,9 @@ function Weather() {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + input + "&units=metric&appid=" + key)
     .then((response) => response.json())
     .then(data => {
-      setCity("City:\t\t" + input);
-      setTemp("Temperature:\t" + data['main']['temp']  + " °C");
-      setDesc("Description:\t" + data['weather'][0]['description']);
+      setCity("City: " + input);
+      setTemp("Temperature: " + data['main']['temp']  + " °C");
+      setDesc("Description: " + data['weather'][0]['description']);
     })
 
     .catch(err => alert("Invalid City Name"))
